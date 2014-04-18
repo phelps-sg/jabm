7531f33369623607e61ed50f89a2b35147c71da6
sphelps <phelps.sg@gmail.com>
2014-04-18 09:21:54 +0100

    migrated to standard maven directory structure

--- jabm/src/net/sourceforge/jabm/event/EventSubscriber.java
+++
@@ -1,9 +0,0 @@
-package net.sourceforge.jabm.event;
-
-import net.sourceforge.jabm.EventScheduler;
-
-public interface EventSubscriber {
-
-	public void subscribeToEvents(EventScheduler scheduler);
-	
-}
--- jabm/src/net/sourceforge/jabm/report/WeightedEdge.java
+++
@@ -1,7 +0,0 @@
-package net.sourceforge.jabm.report;
-
-public interface WeightedEdge {
-
-	public double getValue();
-	
-}
